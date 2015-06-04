c61c8391394b46e5340415587b7e98028784d2ea
Tom Jin <sjin@stats.ox.ac.uk>
2015-06-04 14:04:53 +0100

    Allow arbitary number of genes

--- R/scge.R
+++ R/scge.R
@@ -25,26 +25,21 @@ print.scge <- function(object) {
 }
 
 simulate.scge <- function(object) {
-  param_p <- function(mean, var) {
-    return(mean/var)
-  }
-  param_n <- function(mean, p) {
-    return(mean * p / (1 - p))
-  }
-
-  geneMean <- simulate(object$meanObject, object$nGenes)
-  geneVar  <- simulate(object$varObject, geneMean)
-  geneCensor <- simulate(object$censorObject, geneMean)
-  geneQuant <- simulate(object$copulaObject, object$nGenes)
+  geneMean <- simulate(object$mean, object$nGenes)
+  geneVar  <- simulate(object$var, geneMean)
+  geneCensor <- simulate(object$censor, geneMean)
+  geneQuant <- simulate(object$copula, object$nGenes)
   geneP <- param_p(geneMean, geneVar)
   geneN <- param_n(geneMean, geneP)
 
-  output <- rep(NA, 15122)
-  for (i in 1:15122) {
+  output <- rep(NA, object$nGenes)
+  for (i in 1:object$nGenes) {
     if (geneQuant[i] < geneCensor[i]) {
       output[i] <- 0
     } else {
-      adj_quantile <- pnbinom(0, geneN[i], geneP[i]) + ((1 - pnbinom(0, geneN[i], geneP[i])) * (geneQuant[i] - geneCensor[i]) / (1 - geneCensor[i]))
+      adj_quantile <- pnbinom(0, geneN[i], geneP[i]) +
+        ((1 - pnbinom(0, geneN[i], geneP[i])) * (geneQuant[i] - geneCensor[i]) /
+           (1 - geneCensor[i]))
       output[i] <- qnbinom(adj_quantile, geneN[i], geneP[i])
     }
   }
@@ -54,3 +49,6 @@ simulate.scge <- function(object) {
 summary.scge <- function(object) {
   message("An scge object from package scge.")
 }
+
+param_p <- function(mean, var) {return(mean/var)}
+param_n <- function(mean, p) {return(mean * p / (1 - p))}
