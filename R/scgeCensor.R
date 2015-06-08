54367f6a12da6c455b082b90b3635ac4af9c366b
Tom Jin <sjin@stats.ox.ac.uk>
2015-06-08 18:17:21 +0100

    Sigmoid fitting

--- R/scgeCensor.R
+++ R/scgeCensor.R
@@ -1,16 +1,21 @@
 scgeCensor <- function(data) {
   geneMean <- apply(data, 2, function(x) {mean(x[x != 0])})
   geneCensor <- apply(data, 2, function(x) {sum(x == 0)})
-  #TODO: Fit sigmoid function.
-  object <- list(data = data, geneMean = geneMean, geneCensor = geneCensor, sigmoidScale = -1.4,
-                 position = 5, noiseScale = 0.5)
+
+  d <- data.frame(mean = geneMean, censor = geneCensor/249,
+                  logit.censor = logit(geneCensor/249))[geneCensor != 0,]
+  q <- quantile(geneMean, probs = c(0.5, 1))
+  f <- lm(logit.censor ~ I(log(mean)),d[d$mean > q[1] & d$mean < q[2], ])
+
+  object <- list(data = data, geneMean = geneMean, geneCensor = geneCensor,
+                 scale = coef(f)[2], position = coef(f)[1],
+                 sd = sd(d$censor - sigmoid(coef(f)[1] + coef(f)[2]*log(d$mean))))
   class(object) <- "scgeCensor"
   return(object)
 }
 
 coef.scgeCensor <- function(object) {
-  return(c(position = object$position, sigmoidScale = object$sigmoidScale,
-           noiseScale = object$noiseScale))
+  return(c(position = object$position, scale = object$scale, sd = object$sd))
 }
 
 plot.scgeCensor <- function(object) {
@@ -19,13 +24,16 @@ plot.scgeCensor <- function(object) {
        main = "Sigmoid Fit")
   support <- exp(seq(log(min(object$geneMean)), log(max(object$geneMean)),
                      length.out = 100))
-  lines(support, sigmoid(support, object$sigmoidScale, object$position), col = "blue")
-  lines(support, pmin(1, sigmoid(support, object$sigmoidScale, object$position) + secant(support, object$noiseScale, object$position)), col = "green")
-  lines(support, pmax(0, sigmoid(support, object$sigmoidScale, object$position) - secant(support, object$noiseScale, object$position)), col = "green")
+  lines(support, sigmoid(object$position + object$scale * log(support)),
+        col = "blue")
+  lines(support, pmin(1, sigmoid(object$position + object$scale * log(support)) +
+                        2*object$sd), col = "green")
+  lines(support, pmax(0, sigmoid(object$position + object$scale * log(support)) -
+                        2*object$sd), col = "green")
 }
 
 predict.scgeCensor <- function(object, mean) {
-  return(sigmoid(mean, object$sigmoidScale, object$position))
+  return(sigmoid(object$position + object$scale * log(mean)))
 }
 
 print.scgeCensor <- function(object) {
@@ -34,8 +42,8 @@ print.scgeCensor <- function(object) {
 
 simulate.scgeCensor <- function(object, mean) {
   samples <- sapply(mean, function(m) {
-    sample <- rnorm(1, mean = sigmoid(m, object$sigmoidScale, object$position),
-                    sd = secant(m, object$noiseScale, object$position))
+    sample <- rnorm(1, mean = sigmoid(object$position + object$scale * log(m)),
+                    sd = object$sd)
     sample <- pmax(0, sample)
     sample <- pmin(1, sample)
     sample
@@ -45,10 +53,10 @@ simulate.scgeCensor <- function(object, mean) {
 
 summary.scgeCensor <- function(object) {
   message("Distribution: Sigmoid with normal noise restricted to the [0,1] interval")
-  message("Sigmoid Scale: ", object$sigmoidScale)
-  message("Noise Scale: ", object$noiseScale)
-  message("Position: ", object$position)
+  message("Sigmoid Position: ", object$position)
+  message("Sigmoid Scale: ", object$scale)
+  message("Noise Scale: ", object$sd)
 }
 
-sigmoid <- function(x, scale, position) {1 / (1 + exp(scale * (position - log(x))))}
-secant <- function(x, scale, position) {scale/(exp(log(x) - position) + exp(position - log(x)))}
+logit <- function(x) {-log((1/x) - 1)}
+sigmoid <- function(x) {1 / (1 + exp(-x))}
