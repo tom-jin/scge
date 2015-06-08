58cb0704c393de2b76bdc6ddf8dcc546ac2a4568
Tom Jin <sjin@stats.ox.ac.uk>
2015-06-08 18:39:02 +0100

    Default sigmoid parameters

--- R/scgeCensor.R
+++ R/scgeCensor.R
@@ -1,15 +1,20 @@
-scgeCensor <- function(data) {
-  geneMean <- apply(data, 2, function(x) {mean(x[x != 0])})
-  geneCensor <- apply(data, 2, function(x) {sum(x == 0)})
+scgeCensor <- function(data = NULL) {
+  if(is.null(data)) {
+    object <- list(data = NA, geneMean = NA, geneCensor = NA, scale = -1,
+                   position = 6, sd = 0.2)
+  } else {
+    geneMean <- apply(data, 2, function(x) {mean(x[x != 0])})
+    geneCensor <- apply(data, 2, function(x) {sum(x == 0)})
 
-  d <- data.frame(mean = geneMean, censor = geneCensor/249,
-                  logit.censor = logit(geneCensor/249))[geneCensor != 0,]
-  q <- quantile(geneMean, probs = c(0.5, 1))
-  f <- lm(logit.censor ~ I(log(mean)),d[d$mean > q[1] & d$mean < q[2], ])
+    d <- data.frame(mean = geneMean, censor = geneCensor/249,
+                    logit.censor = logit(geneCensor/249))[geneCensor != 0,]
+    q <- quantile(geneMean, probs = c(0.5, 1))
+    f <- lm(logit.censor ~ I(log(mean)),d[d$mean > q[1] & d$mean < q[2], ])
 
-  object <- list(data = data, geneMean = geneMean, geneCensor = geneCensor,
-                 scale = coef(f)[2], position = coef(f)[1],
-                 sd = sd(d$censor - sigmoid(coef(f)[1] + coef(f)[2]*log(d$mean))))
+    object <- list(data = data, geneMean = geneMean, geneCensor = geneCensor,
+                   scale = coef(f)[2], position = coef(f)[1],
+                   sd = sd(d$censor - sigmoid(coef(f)[1] + coef(f)[2]*log(d$mean))))
+  }
   class(object) <- "scgeCensor"
   return(object)
 }
@@ -19,17 +24,24 @@ coef.scgeCensor <- function(object) {
 }
 
 plot.scgeCensor <- function(object) {
-  plot(object$geneMean, object$geneCensor/nrow(object$data), log = "x",
-       xlab = "Gene Expression Mean", ylab = "Gene Censorship",
-       main = "Sigmoid Fit")
-  support <- exp(seq(log(min(object$geneMean)), log(max(object$geneMean)),
-                     length.out = 100))
+  if (length(object$data) == 1) {
+    plot(1, type = "n", log = "x",
+         xlab = "Gene Expression Mean", ylab = "Gene Censorship",
+         main = "Sigmoid Fit", xlim = c(5, 50000), ylim = c(0, 1))
+    support <- exp(seq(log(5), log(50000), length.out = 100))
+  } else {
+    plot(object$geneMean, object$geneCensor/nrow(object$data), log = "x",
+         xlab = "Gene Expression Mean", ylab = "Gene Censorship",
+         main = "Sigmoid Fit", xlim = c(5, 50000), ylim = c(0, 1))
+    support <- exp(seq(log(min(object$geneMean)), log(max(object$geneMean)),
+                       length.out = 100))
+  }
   lines(support, sigmoid(object$position + object$scale * log(support)),
         col = "blue")
-  lines(support, pmin(1, sigmoid(object$position + object$scale * log(support)) +
-                        2*object$sd), col = "green")
-  lines(support, pmax(0, sigmoid(object$position + object$scale * log(support)) -
-                        2*object$sd), col = "green")
+  lines(support, pmin(1, sigmoid(object$position + object$scale *
+                                   log(support)) + 2*object$sd), col = "green")
+  lines(support, pmax(0, sigmoid(object$position + object$scale *
+                                   log(support)) - 2*object$sd), col = "green")
 }
 
 predict.scgeCensor <- function(object, mean) {
@@ -56,6 +68,11 @@ summary.scgeCensor <- function(object) {
   message("Sigmoid Position: ", object$position)
   message("Sigmoid Scale: ", object$scale)
   message("Noise Scale: ", object$sd)
+  if (length(object$data) == 1) {
+    message("Using default parameters.")
+  } else {
+    message("Parameters fitted to ", nrow(object$data), " observations.")
+  }
 }
 
 logit <- function(x) {-log((1/x) - 1)}
