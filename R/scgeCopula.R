1c32d4d45ae3c33228fac6c7daa77e3c1d67bd38
Tom Jin <sjin@stats.ox.ac.uk>
2015-06-15 14:28:58 +0100

    Use pseudo observations

--- R/scgeCopula.R
+++ R/scgeCopula.R
@@ -7,14 +7,24 @@
 #' @return \code{scgeCopula} returns an object of class "scgeCopula".
 #' @seealso \code{\link{scge}}, \code{\link{scgeMean}}, \code{\link{scgeVar}}
 #' and \code{\link{scgeCensor}}
+#' @references Pollen, A., Nowakowski, T., Shuga, J., Wang, X., Leyrat, A.,
+#' Lui, J., Li, N., Szpankowski, L., Fowler, B., Chen, P., Ramalingam, N.,
+#' Sun, G., Thu, M., Norris, M., Lebofsky, R., Toppani, D., Kemp, D., Wong, M.,
+#' Clerkson, B., Jones, B., Wu, S., Knutsson, L., Alvarado, B., Wang, J.,
+#' Weaver, L., May, A., Jones, R., Unger, M., Kriegstein, A. and West,
+#' J. (2014). \emph{Low-coverage single-cell mRNA sequencing reveals cellular
+#' heterogeneity and activated signaling pathways in developing cerebral cortex.}
+#' Nat Biotechnol, [online] 32(10), pp.1053-1058. Available at:
+#' \url{http://www.nature.com/nbt/journal/v32/n10/abs/nbt.2967.html}
 #' @export
 scgeCopula <- function(data = NULL) {
   if (is.null(data)) {
     object <- list(data = NA, ncol = 1000, chol = cho)
   } else {
-    cor_mat <- cor(data)
+    pdo_obs <- apply(data, 2, rank, na.last = NA)/(nrow(data) + 1)
+    cor_mat <- cor(pdo_obs)
     fix_mat <- Matrix::nearPD(cor_mat, corr = TRUE)$mat
-    cho_mat <- chol(fix_mat)
+    cho_mat <- t(chol(fix_mat))
     object <- list(data = data, ncol = ncol(data), chol = cho_mat)
   }
   class(object) <- "scgeCopula"
@@ -33,7 +43,7 @@ print.scgeCopula <- function(x, ...) {
 
 #' @export
 simulate.scgeCopula <- function(object, nsim = 1, seed = NULL, ...) {
-  return(pnorm(matrix(rnorm(object$ncol * nsim), nsim, object$ncol) %*% object$chol))
+  return(pnorm(object$chol %*% matrix(rnorm(object$ncol * nsim), object$ncol, nsim)))
 }
 
 #' @export
--- tests/testthat/test.scgeCopula.R
+++ tests/testthat/test.scgeCopula.R
@@ -1,8 +1,26 @@
 library(scge)
 context("scgeCopula")
+load(file = "testdata.RData")
 
 defaultObject <- scgeCopula()
+realdataObject <- scgeCopula(data = testdata)
 
 test_that("scgeCopula produces default fit", {
   expect_is(defaultObject, "scgeCopula")
+})
+
+test_that("scgeCopula fits data", {
+  expect_is(realdataObject, "scgeCopula")
+  expect_equal(dim(realdataObject$chol), c(1000, 1000))
+})
+
+test_that("scgeCopula results match copula::normalCopula", {
+  if (!require(copula)) {
+    skip("Package copula not available")
+  }
+
+#   normal.cop <- normalCopula(dim = 100, dispstr = "un")
+#   u <- pobs(testdata[, 1:100])
+#   fit.ml <- fitCopula(normal.cop, u, method = "mpl")
+#   getSigma(fit.ml$copula)
 })
